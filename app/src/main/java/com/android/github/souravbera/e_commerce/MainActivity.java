ee5c056fd90756ff260e75aeef848c8a22ddb9de
saurav <sauravberaiit@gmail.com>
2020-09-15 01:17:11 +0530

    login Activity continue

--- app/src/main/java/com/android/github/souravbera/e_commerce/MainActivity.java
+++ app/src/main/java/com/android/github/souravbera/e_commerce/MainActivity.java
@@ -2,13 +2,38 @@ package com.android.github.souravbera.e_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinNowButton= (Button) findViewById(R.id.main_join_now_button);
+        loginButton= (Button) findViewById(R.id.main_login_button);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent= new Intent(MainActivity.this, LoginActivity.class );
+                startActivity(loginIntent);
+            }
+        });
+
+        joinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent RegisterIntent= new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(RegisterIntent);
+            }
+        });
     }
 }
