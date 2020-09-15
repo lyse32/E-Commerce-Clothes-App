43eb089b1e6c04db63eca0ea34fb21245b797ad6
saurav <sauravberaiit@gmail.com>
2020-09-16 04:23:12 +0530

    Final Order

--- app/src/main/java/com/android/github/souravbera/e_commerce/CartActivity.java
+++ app/src/main/java/com/android/github/souravbera/e_commerce/CartActivity.java
@@ -1,13 +1,28 @@
 package com.android.github.souravbera.e_commerce;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.github.souravbera.e_commerce.Model.Cart;
+import com.android.github.souravbera.e_commerce.Prevalent.Prevalent;
+import com.android.github.souravbera.e_commerce.ViewHolder.CartViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -37,6 +52,84 @@ public class CartActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        final DatabaseReference cartlistRef= FirebaseDatabase.getInstance().getReference().child();
+        final DatabaseReference cartListRef= FirebaseDatabase.getInstance().getReference().child("Cart List");
+
+        FirebaseRecyclerOptions<Cart> options=
+                new FirebaseRecyclerOptions.Builder<Cart>()
+                .setQuery(cartListRef.child("User View")
+                        .child(Prevalent.currentOnlineUser.getPhone()).child("Products"), Cart.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Cart, CartViewHolder> adapter
+                = new FirebaseRecyclerAdapter<Cart, CartViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull CartViewHolder holder, int i, @NonNull final Cart model)
+            {
+                holder.txtProductQuatity.setText("Product Quantity: "+model.getQuantity());
+                holder.txtProductPrice.setText("Product Price: Rs."+model.getPrice());
+                holder.txtProductName.setText("Product: "+model.getProductname());
+
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v)
+                    {
+                            CharSequence  options[]= new CharSequence[]
+                                    {
+                                            "Edit",
+                                            "Remove"
+                                    };
+                        AlertDialog.Builder builder=  new AlertDialog.Builder(CartActivity.this);
+                        builder.setTitle("Cart Options:");
+
+                        builder.setItems(options, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i)
+                            {
+                                if(i==0){
+                                    Intent intent= new Intent(CartActivity.this, ProductsDetailsActivity.class);
+                                    intent.putExtra("pid",model.getPid());
+                                    startActivity(intent);
+                                }
+                                if(i==1)
+                                {
+                                    cartListRef.child("User View")
+                                            .child(Prevalent.currentOnlineUser.getPhone())
+                                            .child(model.getPid())
+                                            .removeValue()
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task)
+                                                {
+                                                    if(task.isSuccessful())
+                                                    {
+                                                        Toast.makeText(CartActivity.this,"Item removed successfully",Toast.LENGTH_SHORT).show();
+
+                                                        Intent intent= new Intent(CartActivity.this, HomeActivity.class);
+                                                        startActivity(intent);
+                                                    }
+
+                                                }
+                                            });
+                                }
+                            }
+                        });
+                        builder.show();
+                    }
+                });
+
+            }
+
+            @NonNull
+            @Override
+            public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_items_layout,parent, false);
+                CartViewHolder holder= new CartViewHolder(view);
+                return holder;
+
+            }
+        };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
     }
 }
