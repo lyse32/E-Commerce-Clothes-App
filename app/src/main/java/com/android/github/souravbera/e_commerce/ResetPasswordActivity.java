285127dac67f9983c19a67201dd0406cb045f746
saurav <sauravberaiit@gmail.com>
2020-09-18 04:51:45 +0530

    Reset Buttons

--- app/src/main/java/com/android/github/souravbera/e_commerce/ResetPasswordActivity.java
+++ app/src/main/java/com/android/github/souravbera/e_commerce/ResetPasswordActivity.java
@@ -1,6 +1,7 @@
 package com.android.github.souravbera.e_commerce;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,11 +9,27 @@ public class ResetPasswordActivity extends AppCompatActivity {
 
     private String check="";
 
+    private TextView
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
 
-        check= getIntent()
+        check= getIntent().getStringExtra("check");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(check.equals("settings"))
+        {
+
+        }
+        else if(check.equals("login"))
+        {
+
+        }
     }
 }
